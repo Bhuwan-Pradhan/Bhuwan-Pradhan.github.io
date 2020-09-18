a81617cecee8a51a26a566cf6ad72a55846c3b58
Bhuwan-Pradhan <71438814+Bhuwan-Pradhan@users.noreply.github.com>
2020-09-18 16:28:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Study Cell</title>
+</head>
+<body>
+    <div id="Header">
+    <nav class="navbar background">
+        <ul class="nav-list">
+            <div class="logo"><img src="img/8818c.jpg" alt="logo"></div>
+            <li><a href="#Home">Home</a></li>
+            <li><a href="#About">About</a></li>
+            <li><a href="#Services">Services</a></li>
+            <li><a href="#Contact">Contact us</a></li>
+        </ul>
+<div class="rightnav">
+    <input type="text" name="Search" id="Search">
+    <button class="btn btn-sm">Search</button>
+</div>
+    </nav>
+    <section class="background firstsection">
+<div class="box-main">
+    <div class="firsthalf">
+        <p class="text-big">Hello ! Jamping japak hfdkgdshf dhskjehfcbeh jeif...</p>
+        <p class="text-small">bhuwan is a good boy  dfhkhfkjskjdis dsfkheh sfvurebu dhsvuerouh djhugrhjh dbhujhhbrihdfgbvdfvur dfjbvguergbjbdfgvjrhg dfjbgrjugbkjdfjgr.</p>
+        <div class="buttons">
+            <button class="btn">Subscribe</button>
+            <button class="btn">Like</button>
+        </div>
+</div>
+    <div class="secondhalf">
+        <img src="img/8818c.jpg" alt="image">
+    </div>
+</div>
+</div>
+<div id="content">
+    </section>
+<section class="section">
+    <div class="para">
+    <p class="sectiontag text-big">JDASFGUGSDAJ DJSHKJHEF DSJKHDHASFU SJDKHFJKHASKJFY </p><hr>
+    <p class="sectionsubtag text-small">ghasdfgdas hjfhjdh kjhfdej hsfbvjdsfh sdvbdjushf sdfj sdvb fh hafjs dkjhasfkjddf jhdjkshfds sfhdkjsdehf ivhf vnkjv fhfjd gdjshfidhsfjkasdhfjhsdfhsjadf dhhf dsha kjshf asdfahskjdhfkjshafashdfkjhsajfk sdfkjhfj fahgahfjfdhhfjhsg ghgkjhgidashfkjdhsg hkjasdhgjhfhkjsdghasghkjsdhgkjsdahghagskjdghjdsghkjsdhgdkjghkjdjgjdshvkjhdjhfafhakj sdghkjadhsghsgfkjhgjhdsjgkhkjasdhgjadshgsdhakjgahejhfkjdsgahsdkjggjkjgfrurehjgbdfgu raghfud dsbugaydf sndbfhdsgaf dskjbgfhgf edskbfhagef dsnfgyaugf sdhjgfaef dsnbfhdjgdegf </p>
+    </div>  
+    <div class="thumbnail">
+    <img src="img/8818c.jpg" alt="thumbnail" class="fluid">
+</div>
+</section>
+<section class="section-left">
+    <div class="para">
+    <p class="sectiontag text-big">JDASFGUGSDAJ DJSHKJHEF DSJKHDHASFU SJDKHFJKHASKJFY </p><hr>
+    <p class="sectionsubtag text-small">ghasdfgdas hjfhjdh kjhfdej hsfbvjdsfh sdvbdjushf sdfj sdvb fh hafjs dkjhasfkjddf jhdjkshfds sfhdkjsdehf ivhf vnkjv fhfjd gdjshfidhsfjkasdhfjhsdfhsjadf dhhf dsha kjshf asdfahskjdhfkjshafashdfkjhsajfk sdfkjhfj fahgahfjfdhhfjhsg ghgkjhgidashfkjdhsg hkjasdhgjhfhkjsdghasghkjsdhgkjsdahghagskjdghjdsghkjsdhgdkjghkjdjgjdshvkjhdjhfafhakj sdghkjadhsghsgfkjhgjhdsjgkhkjasdhgjadshgsdhakjgahejhfkjdsgahsdkjggjkjgfrurehjgbdfgu raghfud dsbugaydf sndbfhdsgaf dskjbgfhgf edskbfhagef dsnfgyaugf sdhjgfaef dsnbfhdjgdegf </p>
+    </div>  
+    <div class="thumbnail">
+    <img src="img/8818c.jpg" alt="thumbnail" class="fluid">
+</div>
+</section>
+<section class="section">
+    <div class="para">
+    <p class="sectiontag text-big">JDASFGUGSDAJ DJSHKJHEF DSJKHDHASFU SJDKHFJKHASKJFY </p><hr>
+    <p class="sectionsubtag text-small">ghasdfgdas hjfhjdh kjhfdej hsfbvjdsfh sdvbdjushf sdfj sdvb fh hafjs dkjhasfkjddf jhdjkshfds sfhdkjsdehf ivhf vnkjv fhfjd gdjshfidhsfjkasdhfjhsdfhsjadf dhhf dsha kjshf asdfahskjdhfkjshafashdfkjhsajfk sdfkjhfj fahgahfjfdhhfjhsg ghgkjhgidashfkjdhsg hkjasdhgjhfhkjsdghasghkjsdhgkjsdahghagskjdghjdsghkjsdhgdkjghkjdjgjdshvkjhdjhfafhakj sdghkjadhsghsgfkjhgjhdsjgkhkjasdhgjadshgsdhakjgahejhfkjdsgahsdkjggjkjgfrurehjgbdfgu raghfud dsbugaydf sndbfhdsgaf dskjbgfhgf edskbfhagef dsnfgyaugf sdhjgfaef dsnbfhdjgdegf </p>
+    </div>  
+    <div class="thumbnail">
+    <img src="img/8818c.jpg" alt="thumbnail" class="fluid">
+</div>
+</section>
+</div>
+<div id="footer">
+    
+</div>
+</body>
+</html>  
